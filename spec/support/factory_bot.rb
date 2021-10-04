302990a2f581ed7376d36e9015395dbf74af92c6
Gregor Wassmann <gregor.wassmann@gmail.com>
2021-10-05 00:56:34 +0200

    Host: Load factories and make specs run

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
+FactoryBot.definition_file_paths << Core::Engine.root.join('spec', 'factories')
+FactoryBot.find_definitions
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
