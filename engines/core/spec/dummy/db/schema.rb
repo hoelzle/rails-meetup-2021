b84c6214eae963443820290569daf679179e3560
Gregor Wassmann <gregor.wassmann@gmail.com>
2021-10-05 00:36:38 +0200

    Core: Move most of the files

---
+++ engines/core/spec/dummy/db/schema.rb
@@ -0,0 +1,41 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_10_04_214450) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "codes", id: false, force: :cascade do |t|
+    t.string "name"
+    t.integer "kind"
+    t.integer "discount"
+    t.index ["name"], name: "index_codes_on_name"
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.integer "discount", limit: 2
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price", precision: 6, scale: 2
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "code_name"
+  end
+
+end
