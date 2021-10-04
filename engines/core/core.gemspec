2b246ad0da3c55416a717d8253f41aee40bf13b5
Gregor Wassmann <gregor.wassmann@gmail.com>
2021-10-05 00:51:16 +0200

    Core: Make specs run

--- engines/core/core.gemspec
+++ engines/core/core.gemspec
@@ -21,4 +21,8 @@ Gem::Specification.new do |spec|
   spec.files = Dir["{app,config,db,lib}/**/*", "MIT-LICENSE", "Rakefile", "README.md"]
 
   spec.add_dependency "rails", "~> 6.1.4", ">= 6.1.4.1"
+
+  spec.add_development_dependency 'factory_bot_rails'
+  spec.add_development_dependency 'rspec-rails'
+
 end
--- engines/core/lib/core/engine.rb
+++ engines/core/lib/core/engine.rb
@@ -1,4 +1,19 @@
 module Core
   class Engine < ::Rails::Engine
+    config.generators do |g|
+      g.api_only = true
+      g.test_framework :rspec
+      g.fixture_replacement :factory_bot
+      g.factory_bot dir: 'spec/factories'
+    end
+
+    # Source: https://tanzu.vmware.com/content/blog/leave-your-migrations-in-your-rails-engines
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths['db/migrate'].expanded.each do |path|
+          app.config.paths['db/migrate'] << path
+        end
+      end
+    end
   end
 end
