c05abaad930cbb793b370449fa6fe932080be2b9
Gregor Wassmann <gregor.wassmann@gmail.com>
2021-10-10 22:33:36 +0200

    Move routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  root to: 'products#index'
-  resources :products, only: %i[show index]
-  resources :decorated_products, only: %i[show index]
+  mount API::Engine => '/api'
 end
--- engines/api/config/routes.rb
+++ engines/api/config/routes.rb
@@ -1,2 +1,5 @@
 API::Engine.routes.draw do
+  root to: 'products#index'
+  resources :products, only: %i[show index]
+  resources :decorated_products, only: %i[show index]
 end
