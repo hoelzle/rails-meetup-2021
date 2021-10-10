5e9ed1698d27363f33490086c4ea0799d872cf8d
Gregor Wassmann <gregor.wassmann@gmail.com>
2021-10-10 22:25:23 +0200

    Make factories run

--- engines/api/lib/api/engine.rb
+++ engines/api/lib/api/engine.rb
@@ -1,5 +1,12 @@
 module API
   class Engine < ::Rails::Engine
     isolate_namespace API
+
+    config.generators do |g|
+      g.api_only = true
+      g.test_framework :rspec
+      g.fixture_replacement :factory_bot
+      g.factory_bot dir: 'spec/factories'
+    end
   end
 end
